9f22ce72842431e66a4168ae27d1dcf293217653
Domenico Visconti <domenico.visconti@prima.it>
2021-12-02 21:55:36 +0100

    make it looks better

--- aoc.el
+++ aoc.el
@@ -3,12 +3,11 @@
 ;;; Day 1
 (defun solution_1 (measuraments)
   "Count the measuraments greater than the previous one in `MEASURAMENTS'."
-  (alist-get
-   'counter
+  (car
    (seq-reduce
     (lambda (acc window-values)
-      (let* ((previuous-elem (alist-get 'previous-element acc))
-             (greater-than-before-counter (alist-get 'counter acc))
+      (let* ((previuous-elem (cdr acc))
+             (greater-than-before-counter (car acc))
              (window-values-sum (seq-reduce (lambda (acc elem) (+ acc elem)) window-values 0))
              (new-previous-elem window-values-sum)
              (counter
@@ -18,21 +17,18 @@
                       previuous-elem))
                   (+ greater-than-before-counter 1)
                 greater-than-before-counter)))
-        (list (cons 'counter counter)
-              (cons 'previous-element new-previous-elem))))
+        (cons counter new-previous-elem)))
     measuraments
-    (list
-     (cons 'counter 0)
-     (cons 'previous-element -1)))))
+    (cons 0 -1))))
 
-(defun group-by-sliding-window (list window-size)
+(defun sliding-window (list window-size)
   "Turn `LIST' into a list of lists, every sub list is created using a sliding window of size `WINDOW-SIZE'."
   (seq-map
    (lambda (index) (seq-subseq list index (+ index window-size)))
    (number-sequence 0 (- (seq-length list) window-size))))
 
 (assert (equal
-         (group-by-sliding-window (list 0 1 2 3 4 5) 3)
+         (sliding-window (list 0 1 2 3 4 5) 3)
          (list (list 0 1 2) (list 1 2 3) (list 2 3 4) (list 3 4 5))))
 
 
@@ -42,6 +38,6 @@
     (insert-file-contents file-path)
     (seq-map (lambda (s) (string-to-number s)) (split-string (buffer-string) "\n"))))
 
-(solution_1 (group-by-sliding-window '(199 200 208 210 200 207 240 269 260 263) 1))
-(solution_1 (group-by-sliding-window (number-list-from-file-content "./day_1.txt") 1))
-(solution_1 (group-by-sliding-window (number-list-from-file-content "./day_1.txt") 3))
+(solution_1 (sliding-window '(199 200 208 210 200 207 240 269 260 263) 1))
+(solution_1 (sliding-window (number-list-from-file-content "./day_1.txt") 1))
+(solution_1 (sliding-window (number-list-from-file-content "./day_1.txt") 3))
