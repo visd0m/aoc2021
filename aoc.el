e836cf752ed1b01b6f142973f4c98ff16410e35f
Domenico Visconti <domenico.visconti@prima.it>
2021-12-03 17:53:48 +0100

    day 2 step 1

--- aoc.el
+++ aoc.el
@@ -1,7 +1,30 @@
 ;;; Advent of code 2021 problems solutions
 
-;;; Day 1
-(defun solution_1 (measuraments)
+;;;;;;;;;;;;; 
+;;; Day 1 ;;;
+;;;;;;;;;;;;;
+
+(defun read-file-as-string (file-path)
+  "Get a string from `FILE-PATH'."
+  (with-temp-buffer
+    (insert-file-contents file-path)
+    (buffer-string)))
+
+(defun number-list-from-file-content (file-path)
+  "Get a list of numbers from `FILE-PATH' content splitting by \n."
+  (seq-map 'string-to-number (split-string (read-file-as-string file-path)) "\n"))
+
+(defun sliding-window (list window-size)
+  "Turn `LIST' into a list of lists, every sub list is created using a sliding window of size `WINDOW-SIZE'."
+  (seq-map
+   (lambda (index) (seq-subseq list index (+ index window-size)))
+   (number-sequence 0 (- (seq-length list) window-size))))
+
+(assert (equal
+         (sliding-window (list 0 1 2 3 4 5) 3)
+         (list (list 0 1 2) (list 1 2 3) (list 2 3 4) (list 3 4 5))))
+
+(defun solution_day_1 (measuraments)
   "Count the measuraments greater than the previous one in `MEASURAMENTS'."
   (car
    (seq-reduce
@@ -20,22 +43,38 @@
     measuraments
     (cons 0 -1))))
 
-(defun sliding-window (list window-size)
-  "Turn `LIST' into a list of lists, every sub list is created using a sliding window of size `WINDOW-SIZE'."
-  (seq-map
-   (lambda (index) (seq-subseq list index (+ index window-size)))
-   (number-sequence 0 (- (seq-length list) window-size))))
+(solution_day_1 (sliding-window '(199 200 208 210 200 207 240 269 260 263) 1))
+(solution_day_1 (sliding-window (number-list-from-file-content "./day_1.in") 1))
+(solution_day_1 (sliding-window (number-list-from-file-content "./day_1.in") 3))
 
-(assert (equal
-         (sliding-window (list 0 1 2 3 4 5) 3)
-         (list (list 0 1 2) (list 1 2 3) (list 2 3 4) (list 3 4 5))))
+;;;;;;;;;;;;; 
+;;; Day 2 ;;;
+;;;;;;;;;;;;;
 
-(defun number-list-from-file-content (file-path)
-  "Get a list of numbers from `FILE-PATH' content splitting by \n."
-  (with-temp-buffer
-    (insert-file-contents file-path)
-    (seq-map 'string-to-number (split-string (buffer-string) "\n"))))
+(defun instruction-from-string (instruction-as-string)
+  "Map a `INSTRUCTION-AS-STRING' into an a-list representing the instruction."
+  (pcase (split-string instruction-as-string " ")
+    (`(,l ,ll)
+     (list (cons 'instruction l) (cons 'value (string-to-number ll))))))
 
-(solution_1 (sliding-window '(199 200 208 210 200 207 240 269 260 263) 1))
-(solution_1 (sliding-window (number-list-from-file-content "./day_1.txt") 1))
-(solution_1 (sliding-window (number-list-from-file-content "./day_1.txt") 3))
+(defun process-instruction (current-position instruction)
+  "Given a `INSTRUCTION' and a `CURRENT-POSITION' modify the current position according to instruction."
+  (let ((instruction (alist-get 'instruction instruction))
+        (value (alist-get 'value instruction)))
+    (pcase instruction
+      (`"forward" (cons (+ (car current-position) value) (cdr current-position)))
+      (`"down" (cons (car current-position) (+ (cdr current-position) value)))
+      (`"up" (cons (car current-position) (- (cdr current-position) value))))))
+
+(defun solution_day_2 (instructions)
+  "Given a set of movement `INSTRUCTIONS', compute them and compute the resulting position."
+  (let ((result (seq-reduce 'process-instruction instructions (cons 0 0))))
+    (* (car result) (cdr result))))
+
+(solution_day_2 (seq-map 'instruction-from-string (split-string (read-file-as-string "./day_2.in") "\n")))
+(solution_day_2 (seq-map 'instruction-from-string (split-string "forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2" "\n")))
