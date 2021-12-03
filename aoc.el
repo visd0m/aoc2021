492fc41a9ec7c8f5cbe0332cafd5b18d3276829e
Domenico Visconti <domenico.visconti@prima.it>
2021-12-03 19:04:43 +0100

    day 2 part 2 + refactoring solution to make it work for both steps

--- aoc.el
+++ aoc.el
@@ -57,24 +57,49 @@
     (`(,l ,ll)
      (list (cons 'instruction l) (cons 'value (string-to-number ll))))))
 
-(defun process-instruction (current-position instruction)
+(defun forward (current-position value use-aim?)
+  "Move forward of `VALUE' `CURRENT-POSITION'."
+  (setf (cdr (assoc 'horizontal current-position)) (+ (alist-get 'horizontal current-position) value))
+  (if use-aim?
+      (setf (cdr (assoc 'depth current-position)) (+ (alist-get 'depth current-position) (* (alist-get 'aim current-position) value))))
+  current-position)
+
+(forward (list (cons 'horizontal 0) (cons 'depth 0) (cons 'aim 1)) 1 t)
+
+(defun down (current-position value use-aim?)
+  "Move down of `VALUE' `CURRENT-POSITION'."
+  (if use-aim?
+      (setf (cdr (assoc 'aim current-position)) (+ (alist-get 'aim current-position) value))
+    (setf (cdr (assoc 'depth current-position)) (+ (alist-get 'depth current-position) value)))
+  current-position)
+
+(defun up (current-position value use-aim?)
+  "Move up of `VALUE' `CURRENT-POSITION'."
+  (if use-aim?
+      (setf (cdr (assoc 'aim current-position)) (- (alist-get 'aim current-position) value))
+    (setf (cdr (assoc 'depth current-position)) (- (alist-get 'depth current-position) value)))
+  current-position)
+
+(defun process-instruction (current-position instruction use-aim?)
   "Given a `INSTRUCTION' and a `CURRENT-POSITION' modify the current position according to instruction."
   (let ((instruction (alist-get 'instruction instruction))
         (value (alist-get 'value instruction)))
     (pcase instruction
-      (`"forward" (cons (+ (car current-position) value) (cdr current-position)))
-      (`"down" (cons (car current-position) (+ (cdr current-position) value)))
-      (`"up" (cons (car current-position) (- (cdr current-position) value))))))
+      (`"forward" (forward current-position value use-aim?))
+      (`"down" (down current-position value use-aim?))
+      (`"up" (up current-position value use-aim?)))))
 
-(defun solution_day_2 (instructions)
-  "Given a set of movement `INSTRUCTIONS', compute them and compute the resulting position."
-  (let ((result (seq-reduce 'process-instruction instructions (cons 0 0))))
-    (* (car result) (cdr result))))
+(defun solution_day_2 (instructions use-aim?)
+  "Given a set of movement `INSTRUCTIONS', and a behaviour modifier `USE-AIM?' compute the resulting position processing instructions."
+  (let ((result (seq-reduce
+                 (lambda (acc elem) (process-instruction acc elem use-aim?))
+                 instructions
+                 (list
+                  (cons 'depth 0)
+                  (cons 'horizontal 0)
+                  (cons 'aim 0)))))
+    (* (alist-get 'horizontal result) (alist-get 'depth result))))
 
-(solution_day_2 (seq-map 'instruction-from-string (split-string (read-file-as-string "./day_2.in") "\n")))
-(solution_day_2 (seq-map 'instruction-from-string (split-string "forward 5
-down 5
-forward 8
-up 3
-down 8
-forward 2" "\n")))
+(solution_day_2 (seq-map 'instruction-from-string (split-string (read-file-as-string "./day_2.in") "\n")) nil)
+(solution_day_2 (seq-map 'instruction-from-string (split-string (read-file-as-string "./day_2.in") "\n")) t)
+
