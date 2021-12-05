f6627e74713821a97cb086dc6557d5f90de7d491
Domenico Visconti <domenico.visconti@prima.it>
2021-12-05 18:10:00 +0100

    day 3 part 1

--- aoc.el
+++ aoc.el
@@ -103,3 +103,67 @@
 (solution_day_2 (seq-map 'instruction-from-string (split-string (read-file-as-string "./day_2.in") "\n")) nil)
 (solution_day_2 (seq-map 'instruction-from-string (split-string (read-file-as-string "./day_2.in") "\n")) t)
 
+;;;;;;;;;;;;; 
+;;; Day 3 ;;;
+;;;;;;;;;;;;;
+
+(defun nth-list (list-of-lists index)
+  (seq-map (lambda (list) (nth index list)) list-of-lists))
+
+(defun most-or-least-common-bit (bits most-or-least)
+  (let* ((counters (seq-reduce
+                    (lambda (acc elem)
+                      (if (eq elem ?0)
+                          (cons (+ (car acc) 1) (cdr acc))
+                        (cons (car acc) (+ (cdr acc) 1))))
+                    bits
+                    (cons 0 0)))
+         (0-counter (car counters))
+         (1-counter (cdr counters)))
+    (pcase most-or-least
+      ('most
+       (if (> 0-counter 1-counter)
+           "0"
+         "1"))
+      ('least
+       (if (> 0-counter 1-counter)
+           "1"
+         "0")))))
+
+(most-or-least-common-bit
+ (nth-list
+  (seq-map 'string-to-list (diagnostic-report-from-input-file "./day_3.in"))
+  4)
+ 'least)
+
+(defun rate (list-of-bits-lists gamma-or-epsilon)
+  (let ((most-or-least (if (eq gamma-or-epsilon 'gamma)
+                           'most
+                         'least))
+        (number-of-bits (- (length (nth 0 list-of-bits-lists)) 1)))
+    (seq-reduce
+     (lambda (acc elem)
+       (format "%s%s" acc (most-or-least-common-bit (nth-list list-of-bits-lists elem) most-or-least)))
+     (number-sequence 0 number-of-bits)
+     "")))
+
+(defun solution_day_3 (diagnostic-report)
+  (let* ((list-of-byte-lists (seq-map 'string-to-list diagnostic-report))
+         (gamma-rate (string-to-number (rate list-of-byte-lists 'gamma) 2))
+         (epsilon-rate (string-to-number (rate list-of-byte-lists 'epsilon) 2)))
+    (* gamma-rate epsilon-rate)))
+
+(solution_day_3 (split-string (read-file-as-string "./day_3.in") "\n"))
+(solution_day_3 (split-string "00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010" "\n"))
+
