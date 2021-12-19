111d0565e2790da9cb9f4d02419a06aa5a64219c
Domenico Visconti <domenico.visconti@prima.it>
2021-12-19 18:44:13 +0100

    Add documentation to common functions

--- list.el
+++ list.el
@@ -1,7 +1,9 @@
 (defun nth-list (list-of-lists index)
+  "Get a list composed by all the `INDEX elements of the lists contained in `LIST-OF-LISTS."
   (seq-map (lambda (list) (nth index list)) list-of-lists))
 
 (defun update-nth-in-list (list nth new-value &optional step)
+  "Update the `NTH value of the `LIST replacing it with `NEW-VALUE."
   (let ((step (or step 0)))
     (if (equal step nth)
         (progn
